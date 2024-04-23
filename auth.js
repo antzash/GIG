773676f79820e49d2750d200f8f4f46b38fc8249
antzash <antony.hashwin@gmail.com>
2024-04-23 13:45:28 +0800

    adding jwt and login

--- auth.js
+++ auth.js
@@ -1,7 +1,15 @@
 const jwt = require("jsonwebtoken");
 const secret = "your_jwt_secret"; // Use an environment variable in production
 
+const jwt = require("jsonwebtoken");
+
 function generateToken(user) {
+  const secret = process.env.JWT_SECRET; // Make sure this is correctly referencing the environment variable
+  if (!secret) {
+    throw new Error(
+      "JWT secret is not set. Check your .env file and environment."
+    );
+  }
   return jwt.sign({ userId: user.id }, secret, { expiresIn: "24h" });
 }
 
