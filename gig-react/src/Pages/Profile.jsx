587a303535c0510821898fff7881da1aaace0f58
antzash <antony.hashwin@gmail.com>
2024-04-23 22:13:12 +0800

    fetch right user details

--- gig-react/src/Pages/Profile.jsx
+++ gig-react/src/Pages/Profile.jsx
@@ -2,26 +2,42 @@ import React, { useContext, useEffect } from "react";
 import { useUser } from "../Context/UserContext";
 
 function ProfilePage() {
-  const { user, fetchUserProfile } = useUser();
-
-  // useEffect(() => {
-  //   fetchUserProfile();
-  // }, [fetchUserProfile]);
+  const { user } = useUser();
 
   return (
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4">Profile Details</h2>
       <div className="bg-white shadow rounded p-4">
-        <p className="mb-2">
-          <span className="font-bold">Band Name:</span>{" "}
-          {user.details?.band_name}
-        </p>
-        <p className="mb-2">
-          <span className="font-bold">Genre:</span> {user.details?.genre}
-        </p>
-        <p className="mb-2">
-          <span className="font-bold">Bio:</span> {user.details?.bio}
-        </p>
+        {user.role === "artist" ? (
+          <>
+            <p className="mb-2">
+              <span className="font-bold">Band Name:</span>{" "}
+              {user.details?.band_name}
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">Genre:</span> {user.details?.genre}
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">Bio:</span> {user.details?.bio}
+            </p>
+          </>
+        ) : user.role === "venue" ? (
+          <>
+            <p className="mb-2">
+              <span className="font-bold">Venue Name:</span>{" "}
+              {user.details?.venue_name}
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">Address:</span>{" "}
+              {user.details?.address}
+            </p>
+            <p className="mb-2">
+              <span className="font-bold">Bio:</span> {user.details?.bio}
+            </p>
+          </>
+        ) : (
+          <p>No profile details available.</p>
+        )}
       </div>
     </div>
   );
