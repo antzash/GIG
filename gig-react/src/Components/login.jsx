1745600571a9eb44f00f9759220211645849b052
antzash <antony.hashwin@gmail.com>
2024-04-30 10:23:08 +0800

    undo changes

--- gig-react/src/Components/login.jsx
+++ gig-react/src/Components/login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useUser } from "../Context/UserContext";
-s;
 import gigLogo from "../gig-logo.png";
 
 function LoginForm() {
